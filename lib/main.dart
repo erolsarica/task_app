d58ec374ed34cb354a5f14073247fa22206c76ea
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2024-01-27 22:57:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {dd
     return MaterialApp(
       title: '',
       theme: ThemeData(
